295433dadd51d84759adbf3e65eff43fd2d54242
Yael Rosen <[yaelr@vt.edu]>
2026-02-01 21:15:04 -0500

    hw 2

--- hw2/sketch.js
+++ hw2/sketch.js
@@ -1,24 +1,73 @@
-function setup () {
-    let canvas = createCanvas(400,400, WEBGL);
+function setup() {
+    let canvas = createCanvas(400, 400, WEBGL);
     angleMode(DEGREES);
 }
 
-function draw () {
+function draw() {
     background(0);
     orbitControl();
+    //top left sphere
+    push();
     stroke(255);
-    fill(255,0,0);
-    box(100);
-    translate(0,0,100);
-    fill(0,255,0);
-    box(50);
-    translate(0,0,50);
-    rotateY(45);
-    rotateX(45);
-    fill(0,0,225);
-    box(25);
-    translate(100,0,0);
-    scale(0.5);
-    fill(0);
-    box(25);
+    fill(66, 135, 245);
+    translate(-150, -150, 0);
+    sphere(30);
+    pop();
+    //bottom right sphere
+    push();
+    stroke(255);
+    fill(66, 135, 245);
+    translate(150, 150, 0);
+    sphere(30);
+    pop();
+    //top right sphere
+    push();
+    stroke(255);
+    fill(66, 135, 245);
+    translate(150, -150, 0);
+    sphere(30);
+    pop();
+    //bottom left sphere
+    push();
+    stroke(255);
+    fill(66, 135, 245);
+    translate(-150, 150, 0);
+    sphere(30);
+    pop();
+    //right cylinder
+    push();
+    stroke(255);
+    fill(191, 66, 245);
+    translate(150, 0, 0);
+    cylinder(15, 70);
+    pop();
+    //left cylinder
+    push();
+    stroke(255);
+    fill(191, 66, 245);
+    translate(-150, 0, 0);
+    cylinder(15, 70);
+    pop();
+    //bottom cylinder
+    push();
+    stroke(255);
+    fill(191, 66, 245);
+    translate(0, 150, 0);
+    rotateZ(90);
+    cylinder(15, 70);
+    pop();
+    //top cylinder
+    push();
+    stroke(255);
+    fill(191, 66, 245);
+    translate(0, -150, 0);
+    rotateZ(90);
+    cylinder(15, 70);
+    pop();
+    //ring
+    push();
+    stroke(255);
+    fill(245, 206, 66);
+    torus(30);
+    pop();
 }
