0c11cdf7fba3c7b0c73fb6821e6a06979054fd94
Yael Rosen <[yaelr@vt.edu]>
2026-02-09 09:46:45 -0500

    finish hw3

--- hw3/sketch.js
+++ hw3/sketch.js
@@ -2,35 +2,53 @@ let myShape;
 
 function setup() {
     let canvas = createCanvas(400, 400, WEBGL);
-    angleMode(DEGREES);
-    //ballBlob();
+    background(0);
+    rose();
+    rose();
+    rose();
+    rose();
 }
 
 function draw() {
-    background(0);
+    //rose();
+    //rose();
+    //rose();
+    //rose();
+}
+
+function rose() {
+    angleMode(DEGREES);
     orbitControl();
     noStroke();
     lights();
-    
+
     fill(9, 135, 79);
     shininess(10);
-    //specularMaterial(255,0,255);
+    specularMaterial(9, 135, 79);
     //emissiveMaterial(255,0,255);
     cylinder(5, 150);
-    //filter(POSTERIZE, 1);
-    //model(myShape);
-    
+
     fill(224, 33, 16);
+    specularMaterial(224, 33, 16);
     translate(0, -100, 0);
     sphere(30, 6);
-       
-    fill(171, 114, 39);
-    translate(0,170,0);
-    box(70, 20);
-    translate(0,30,0);
-    box(50, 60, 20);
-    
-    translate(0,-70,0);
-    cone(30, 50);
 
+    fill(171, 114, 39);
+    specularMaterial(171, 114, 39);
+    translate(0, 170, 0);
+    box(70, 20);
+    translate(0, 30, 0);
+    box(50, 60, 20);
+
+    fill(9, 135, 79);
+    specularMaterial(9, 135, 79);
+    translate(-15, -80, 0);
+    rotateZ(90);
+    cone(5, 25);
+    translate(-30, -30, 0);
+    rotateZ(180);
+    cone(5, 25);
+    translate(30, -30, 0);
+    rotateZ(180);
+    cone(5, 25);
 }
