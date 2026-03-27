b7ee0e04be3a5f5e2c48d8f3ea21ae1bc2225174
Yael Rosen <[yaelr@vt.edu]>
2026-03-27 12:13:46 -0400

    start hw 6

---
+++ hw6/script.js
@@ -0,0 +1,78 @@
+// Basic Three.js Example
+// Chelsea Thompto - Spring 2026
+
+// Three.js uses an import map to add features.
+// The "import * as THREE from 'three';" will be
+// in all sketches. Add-ons will be added after.
+
+// The main library script
+import * as THREE from "three";
+
+// The plug-in for orbit controls
+import { OrbitControls } from "./src/OrbitControls.js";
+
+// Declaring global variables.
+let camera, canvas, controls, scene, renderer;
+
+// Run the "init" function which is like "setup" in p5.
+init();
+
+// Define initial scene
+function init() {
+    // scene setup
+    canvas = document.getElementById("3-holder");
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xbfeff5);
+    scene.fog = new THREE.FogExp2(0xbfeff5, 0.0015);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    //renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(400, 400);
+    renderer.setAnimationLoop(animate);
+    canvas.appendChild(renderer.domElement);
+
+    // Setup camera
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(400, 200, 0);
+
+    // Setup controls
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.listenToKeyEvents(window);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+    controls.screenSpacePanning = false;
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
+    controls.cursorStyle = "grab";
+    controls.maxPolarAngle = Math.PI / 2;
+
+    // Add world geometry
+    const coneGeometry = new THREE.CylinderGeometry(50, 50, 300);
+    const material1 = new THREE.MeshPhongMaterial({ color: 0x0a5e2e, flatShading: true });
+    const mesh3 = new THREE.InstancedMesh(coneGeometry, material1, 500);
+    mesh3.position.set(-100, 10, 130);
+    scene.add(mesh3);
+
+    // lights
+    const dirLight1 = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight1.position.set(1, 1, 1);
+    scene.add(dirLight1);
+
+    const dirLight2 = new THREE.DirectionalLight(0xffffff, 2);
+    dirLight2.position.set(-1, -1, -1);
+    scene.add(dirLight2);
+
+    const ambientLight = new THREE.AmbientLight(0x555555);
+    scene.add(ambientLight);
+}
+
+// Function to update moving objects, in this case the camera.
+// The render function is trigger at the end to update the canvas.
+function animate() {
+    controls.update();
+    render();
+}
+
+// Function to render the scene using the camera.
+function render() {
+    renderer.render(scene, camera);
+}
